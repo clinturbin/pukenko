865518c7fdebf5e790b57cc328ff82de183cfd47
Steven Berk <steven.e.berk@gmail.com>
2018-08-29 13:51:20 -0400

    buttons now change scores

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
+var happyScoreIntoDom = document.querySelector('.HappyScore');
+var healthScoreIntoDom = document.querySelector('.HealthScore');
+var conductScoreIntoDom = document.querySelector('.ConductScore');
 
-function randomHappyScore(min, max) {
+var smoothieButton = document.querySelector('.smoothieButton');
+var junkfoodButton = document.querySelector('.junkfoodButton');
+var homeworkButton = document.querySelector('.homeworkButton');
+
+function randomScore(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
  };
- 
- function randomHealthScore(min, max) {
-     return Math.floor(Math.random() * (max - min) + min);
-  };
- 
- function randomConductScore(min, max) {
-     return Math.floor(Math.random() * (max - min) + min);
-  };
- 
- var defaultHappyScore = randomHappyScore(2,8);
- var defaultHealthScore = randomHealthScore(2,8);
- var defaultConductScore = randomConductScore(2,8);
- 
-//inserts happy score - for my own reference, should come from DB in final version
- var happyScoreIntoDom = document.querySelector('.HappyScore');
- var happyScore = document.createElement('p');
- happyScore.textContent = defaultHappyScore;
- happyScoreIntoDom.appendChild(happyScore);
 
-//inserts health score - for my own reference, should come from DB in final version
- var healthScoreIntoDom = document.querySelector('.HealthScore');
- var healthScore = document.createElement('p');
- healthScore.textContent = defaultHealthScore;
- healthScoreIntoDom.appendChild(healthScore);
+let defaultHappyScore = randomScore(2,8);
+let defaultHealthScore = randomScore(2,8);
+let defaultConductScore = randomScore(2,8);
 
- //inserts conduct score - for my own reference, should come from DB in final version
- var conductScoreIntoDom = document.querySelector('.ConductScore');
- var conductScore = document.createElement('p');
- conductScore.textContent = defaultConductScore;
- conductScoreIntoDom.appendChild(conductScore);
+let updateScores = function(){
+    healthScoreIntoDom.textContent = defaultHealthScore;
+    happyScoreIntoDom.textContent = defaultHappyScore;
+    conductScoreIntoDom.textContent = defaultConductScore;
+}
+ 
+
+smoothieButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    defaultHealthScore += 1;
+    updateScores();
+    console.log("smoothie");
+});
+
+junkfoodButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    defaultHealthScore -= 1;
+    defaultHappyScore += 3;
+    defaultConductScore -= 1;
+    updateScores();
+    console.log("junkfood");
+});
+
+homeworkButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    defaultConductScore += 1;
+    defaultHappyScore -= 1;
+    updateScores();
+   console.log("homework");
+});
+
+
+
+
+
+
+updateScores();
